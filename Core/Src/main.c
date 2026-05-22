e531594d04cc3e68cf518881a6126dbbd22785e0
Mirfa <151968120+mirfa2@users.noreply.github.com>
2026-05-22 19:04:15 +0200

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <string.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -59,9 +59,59 @@ static void MX_USART2_UART_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-	//if we dont know how much total_data to receive (total_data including the header data like the data_length data)
-	//eg continuous data stream, or if the mcu doesnt have enough sram to store the data eg; receive video in Mbytes
-	//
+	//if we dont know how much total_data to receive (eg continuous data stream), or if the mcu doesnt have
+	//enough sram to store the data (eg; receive video in Mbytes) and we should store the received data to
+	//other external memory (like sdcard connected to the mcu), then we can receive in dma circular mode and
+	//continuously write the data, in portions, to the external memory using halftransfer and completetransfer
+	//callback methods, like in uart tx dma. we STILL expect to receive the data_length data like before
+
+	//f466re has 512 kilobytes of non-volatile flash memory (we it stores compiled c codes, etc)
+	//and 128 kilobytes of SRAM, volatile temporary working memory
+
+uint8_t RxData[256];	//main buffer to temporarily store the received data in portions in the sram.
+uint8_t externalMemory[4096];	//represents our external memory for this demo's sake
+
+int isSizeReceived = 0;	//flag var to keep track if we have receive the data_length data
+uint16_t dataLength=0;	//data_length data
+
+int HTC = 0;	//half transfer complete flag var
+int FTC = 0;	//full transfer complete flag var
+uint16_t indx=0;	//index var to keep track on how much data is already transferred to the external memory
+					//also a pointer to where we store the data in the external memory
+
+void HAL_UART_RxHalfCpltCallback(UART_HandleTypeDef *huart)
+{
+	//first receive and store the data_length, then transfer the received data in the first half buffer to external memory
+	if (isSizeReceived == 0)
+	{
+		dataLength = ((RxData[0]-48)*1000)+((RxData[1]-48)*100)+((RxData[2]-48)*10)+((RxData[3]-48));  //store data_length
+		indx = 0;	//to store the data at the start of the external memory
+		memcpy(externalMemory+indx, RxData+4, 124);  // copy the data from the mcu receive buffer into the external memory
+		memset(RxData, '\0', 128);  // clear the first half of the receive buffer, half buffer size = 128 bytes
+		indx += 124;  //update index. we transferred 124 bytes of the received data to external mem
+		isSizeReceived = 1;  //on the next half transfer we should continue receiving and transfering the data packet
+	}
+	else	//continue receiving the data and transferring the data in first half buffer to external memory
+	{
+		memcpy(externalMemory+indx, RxData, 128);	//now we can copy the entire half buffer because now all bytes are data bytes
+		memset(RxData, '\0', 128);
+		indx += 128;
+	}
+
+	//set the transfer flags
+	HTC=1;
+	FTC=0;
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	  memcpy(externalMemory+indx, RxData+128, 128);	//transfer the 2nd half of the receive buffer
+	  memset(RxData+128, '\0', 128);	//clear the 2nd half of the receive buffer
+	  indx+=128;
+
+	  HTC=0;
+	  FTC=1;
+}
 
 /* USER CODE END 0 */
 
@@ -98,6 +148,9 @@ int main(void)
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
 
+  //dma rx in circularmode, receive data in chunks of 256 bytes
+  HAL_UART_Receive_DMA(&huart2, RxData, 256);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -108,7 +161,46 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 
-	  //blink LED every 500ms
+//since we define our receive buffer in 256 chunks, the HTC and FTC will only trigger when the total_data is a multiple of 256.
+//we need to handle the case when total_data isnt a multiple of 256, meaning there are remaining bytes in the mcu receive buffer
+//that we need to manually transfer to the external memory. "datalength-indx" equals 0 when all data in the packet is
+//transferred, at the start of the transfer it is naturally >0, then at the end, it should be <128 because it is now the remaider
+//of the transfer. we transfer data at 128 chunks from receive buffer to external memory
+
+	  if (  ((dataLength-indx)>0) && ((dataLength-indx)<128)  )
+	  {
+		  //remaining bytes in the 2nd half of the buffer
+		  if (HTC==1)
+		  {
+			  //strcpy((char *)externalMemory+indx, (char *)RxData+128);
+			  memcpy(externalMemory+indx, RxData+128, (dataLength-indx));	//copy the remainder to external memory
+			  indx = dataLength;	//transfer complete
+
+			  //set up dma transfer for the next packet
+			  isSizeReceived = 0;
+			  HTC = 0;
+
+			  //because dma in circular mode, we need to restart it first so that the next dma will store data starting at the
+			  //start of the receive buffer, not continuing from the previous position
+			  HAL_UART_DMAStop(&huart2);
+			  HAL_UART_Receive_DMA(&huart2, RxData, 256);
+		  }
+
+		  //remaining bytes in the 1st half of the buffer
+		  else if (FTC==1)
+		  {
+			  //strcpy((char *)externalMemory+indx, (char *)RxData);	because we cleared the buffer we can use strcpy() too
+			  memcpy(externalMemory+indx, RxData, (dataLength-indx));
+			  indx = dataLength;
+			  isSizeReceived = 0;
+			  FTC = 0;
+			  HAL_UART_DMAStop(&huart2);
+			  HAL_UART_Receive_DMA(&huart2, RxData, 256);
+		  }
+	  }
+
+
+	  //blink LED every 500ms, cpu free cause dma
 	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
 	  HAL_Delay(500);
 
