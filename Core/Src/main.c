0c0876fbdecd11961449c8682a984e7e3da218fd
Mirfa <151968120+mirfa2@users.noreply.github.com>
2026-05-22 17:10:07 +0200

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -103,6 +103,11 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+
+	  //blink LED every 500ms
+	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	  HAL_Delay(500);
+
   }
   /* USER CODE END 3 */
 }
